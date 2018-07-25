95cc49ecffff93351030556b8ffbc1b616e903af
胡思楷 <husk@corp.netease.com>
2018-07-25 18:03:52 +0800

    修改源码

--- getLocalIdentName.js
+++ getLocalIdentName.js
@@ -1,7 +1,7 @@
 module.exports = path => {
   const antdProPath = path.match(/src(.*)/)[1].replace('.less', '');
   const arr = antdProPath
-    .split('/|\\')
+    .split('\/|\\')
     .map(a => a.replace(/([A-Z])/g, '-$1'))
     .map(a => a.toLowerCase());
   return `antd-pro${arr.join('-')}-`.replace(/--/g, '-');
